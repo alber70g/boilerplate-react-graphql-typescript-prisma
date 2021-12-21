1554e6e800ce89832c2ede7b655c665234d31686
Albert Groothedde <albertgroothedde@gmail.com>
2021-12-21 11:51:17 +0100

    chore: ALBERT ANDERE LAPTOP

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,17 @@
 import { ApolloServer } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+
 import { typeDefs, resolvers } from './schema';
+
+const prisma = new PrismaClient()
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: {prisma} });
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
+}).finally(() => {
+    prisma.$di
 });
